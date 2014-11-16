5a9916ffcf077d0231ce04a04a99e8d22f2ef06e
lonesomedreams <bunnyerida@gmail.com>
2014-11-16 17:38:26 +0800

    Updated plugins and libraries

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,12 @@
     <!--./ FOOTER SECTION END -->
     <!--  Jquery Core Script -->
-    <script src="<?php echo base_url();?>assets/js/jquery-1.10.2.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
     <!--  Core Bootstrap Script -->
     <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
      <!--  WOW Script -->
     <script src="<?php echo base_url();?>assets/js/wow.min.js"></script>
     <!--  Scrolling Script -->
-    <script src="<?php echo base_url();?>assets/js/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.easing.1.3.js"></script>
     <!--  PrettyPhoto Script -->
     <script src="<?php echo base_url();?>assets/js/jquery.prettyPhoto.js"></script>
     <!--  Custom Scripts -->
