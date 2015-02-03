aa43cf4f4e9db6966ea5a50010ee8e992fb2e516
christine14 <carin.christine14@gmail.com>
2015-02-03 23:27:38 +0800

    Header change
    Teacher Header

--- application/views/teacher/header.php
+++ application/views/teacher/header.php
@@ -101,7 +101,13 @@
     <div class="navbar navbar-fixed-top">
         <div class="navbar-inner">
             <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">OBE - SAMS</a>
+
+                <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a>
+                <a class="brand" href="index.html">
+                <img src="<?php echo base_url();?>assets/img/obesams.png" class="navbar-brand-logo " alt="" />
+                
+                </a> 
+
                 <div class="nav-collapse">
                     <ul class="nav pull-right">
                         <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
