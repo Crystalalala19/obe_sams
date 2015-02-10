f1b074966978299f7a5ffff73f720a3efeda9444
anjylique <angelequegrey@gmail.com>
2015-02-10 11:35:58 +0800

    student account
    scorecard

--- application/controllers/student.php
+++ application/controllers/student.php
@@ -12,9 +12,44 @@ class Student extends CI_Controller {
         $data['user'] = $this->model_student->select_user();
         $data['title'] = 'OBE SAMS Academic';
 
+        $student_id = $this->session->userdata('idnum');
+
+        $data['get_studentName'] = $this->model_student->get_studentName($student_id);
+        $data['get_EY'] = $this->model_student->get_scoreEY($student_id);
+        $data['get_class'] = $this->model_student->get_class($student_id);
+        
+        $data['class_list'] = $this->model_student->select_classSC($student_id);
+        $multi_array = array();
+        $i = 0;
+
+        foreach ($data['class_list'] as $key2 => $val2) {
+            $multi_array[$i++] = $this->model_student->get_po_courseSC($val2['courseID']);
+        }
+
+        $data['m_array'] = $multi_array[0];
+        foreach($data['class_list'] as $key => $val) {
+            $data['class_list'][$key]['score'] = $this->model_student->get_po_score($val['classID'], $student_id);
+            $data['class_list'][$key]['grade'] = array();
+            $data['class_list'][$key]['poID'] = $this->model_student->get_student_POID($val['classID'], $student_id);
+            $i = 0;
+
+            foreach($data['m_array'] as $key1 => $val1) {
+                if($val1['status'] == "1"  && isset($data['class_list'][$key]['score'][$i])) {
+                    $data['class_list'][$key]['grade'][$i] =  $data['class_list'][$key]['score'][$i]['score'];
+                } else {
+                    $data['class_list'][$key]['grade'][$i] = "";
+                }
+
+                $i++;
+            }
+        }
+
+
         $this->load->view('student/header', $data);
-        $this->load->view('student/index');
+        $this->load->view('student/index', $data);
         $this->load->view('student/footer');
+
+
     }
     
 }
--- application/models/model_student.php
+++ application/models/model_student.php
@@ -12,5 +12,55 @@ class Model_student extends CI_Model {
         return $query->result_array();
     }
 
+     function get_scoreEY($student_id) {
+        $query = $this->db->query("SELECT * FROM student_effectiveyear INNER JOIN program_year ON student_effectiveyear.pyID = program_year.ID
+                                                        INNER JOIN program ON program_year.programID = program.ID
+                                                        WHERE student_id = '".$student_id."' ");
+        
+        return $query->result();
+    }
+
+    function get_studentName($student_id) {
+        $query = $this->db->query("SELECt * FROM student WHERE student_id = '".$student_id."' ");
+        return $query->result();
+    }
+
+    function get_class($student_id) {
+        $query = $this->db->query("SELECT DISTINCT semester, school_year FROM student_course 
+                                    INNER JOIN teacher_class ON student_course.classID = teacher_class.ID
+                                    WHERE studentID = '".$student_id."' ");
+        return $query->result();
+    }
+
+    function select_classSC($student_id) {
+        $query = $this->db->query("SELECT * FROM student_course 
+                                            INNER JOIN teacher_class ON student_course.classID = teacher_class.ID
+                                            WHERE studentID = '".$student_id."' 
+                                            GROUP BY classID");
+
+        return $query->result_array();
+    }
+
+    function get_po_courseSC($courseID) {
+        $query = $this->db->query("SELECT * FROM po_course WHERE courseID = '".$courseID."' ");
+        
+        return $query->result_array();
+    }
+
+    function get_po_score($class_id, $student_id){
+        $query = $this->db->query("SELECT score FROM student_course
+                                WHERE studentID = '".$student_id."' AND classID = '".$class_id."' ");
+
+        return $query->result_array();
+    }
+    
+    function get_student_POID($class_id, $student_id) {
+        $query = $this->db->query("SELECT poID FROM student_course
+                                WHERE studentID = '".$student_id."' AND classID = '".$class_id."' ");
+
+        return $query->result_array();
+    }
+
+
 }
 ?>    
