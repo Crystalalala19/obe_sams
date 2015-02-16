526820ad67db68f03682d81523404497246001d9
lonesomedreams <bunnyerida@gmail.com>
2015-02-16 21:02:07 +0800

    Minor button

--- application/views/admin/view_report_teacher.php
+++ application/views/admin/view_report_teacher.php
@@ -69,7 +69,7 @@
                                         <td><?php echo $row['courseCode'];?></td>
                                         <td><?php echo $row['start_time'].' - '. $row['end_time'].' '. $row['days'];?></td>
                                         <td>
-                                            <a type="button" title="View Classes" class="btn btn-warning btn-small btn-responsive" href="<?php echo base_url();?>admin/teachers/scorecard/<?php echo $row['teacher_id'].'/'.$row['school_year'].'/'.$row['ID'];?>"><i class="icon-book"></i></a>
+                                            <a type="button" title="View Classes" class="btn btn-warning btn-small btn-responsive" href="<?php echo base_url();?>admin/teachers/scorecard/<?php echo $row['teacher_id'].'/'.$row['school_year'].'/'.$row['ID'];?>">View Classes</a>
                                         </td>
                                     </tr>
                                     <?php endforeach;?>
