8c7bbff2112ddfb5912e45dbc3da5f9c47d4c9a3
lonesomedreams <bunnyerida@gmail.com>
2015-02-19 14:49:34 +0800

    Back button works after logout, fixed

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -159,6 +159,11 @@ class Admin extends CI_Controller {
     }
 
     function check_role() {
+        header("cache-Control: no-store, no-cache, must-revalidate");
+        header("cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+        
         if($this->session->userdata('role') != 'admin')
             redirect('site');
     }
